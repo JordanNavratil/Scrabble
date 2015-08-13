2d51314677661d8951d6b80f75ebcd341c2c1f2c
Jason Bethel & jordan navratil <student@epicodus.com>
2015-08-13 11:52:38 -0700

    finish method

--- src/Scrabble.php
+++ src/Scrabble.php
@@ -8,7 +8,10 @@
             $group3 = ['b', 'c', 'm', 'p'];
             $group4 = ['f', 'h', 'v', 'w', 'y'];
             $group5 = ['k'];
+            $group6 = ['j', 'x'];
+            $group7 = ['q', 'z'];
             $result = 0;
+            $user_word = strtolower($user_word);
             $user_letters = str_split($user_word);
 
             foreach ($user_letters as $letter) {
@@ -20,16 +23,18 @@
                     $result = $result +3;
                 } elseif (in_array($letter, $group4)) {
                     $result = $result +4;
-                } elseif (in_array($letter, $group)) {
+                } elseif (in_array($letter, $group5)) {
                     $result = $result +5;
-
+                } elseif (in_array($letter, $group6)) {
+                    $result = $result +8;
+                } elseif (in_array($letter, $group7)) {
+                    $result = $result +10;
+                } else {
+                    $result = 'This is not a word';
                 }
-
-
-            }
+        }
             return $result;
         }
-
     }
 
 
--- tests/ScrabbleTest.php
+++ tests/ScrabbleTest.php
@@ -7,7 +7,7 @@
         {
             //Arrange
             $test_Scrabble = new Scrabble;
-            $user_word = "a";
+            $user_word = "A";
 
             //Act
             $result = $test_Scrabble->findScore($user_word);
@@ -54,6 +54,59 @@
             //Assert
             $this->assertEquals("10", $result);
         }
+
+        function test_findScore_fiveLetter()
+        {
+            //Arrange
+            $test_Scrabble = new Scrabble;
+            $user_word = "egchk";
+
+            //Act
+            $result = $test_Scrabble->findScore($user_word);
+
+            //Assert
+            $this->assertEquals("15", $result);
+        }
+
+        function test_findScore_sixLetter()
+        {
+            //Arrange
+            $test_Scrabble = new Scrabble;
+            $user_word = "igbvkj";
+
+            //Act
+            $result = $test_Scrabble->findScore($user_word);
+
+            //Assert
+            $this->assertEquals("23", $result);
+        }
+
+        function test_findScore_sevenLetter()
+        {
+            //Arrange
+            $test_Scrabble = new Scrabble;
+            $user_word = "zjkypgt";
+
+            //Act
+            $result = $test_Scrabble->findScore($user_word);
+
+            //Assert
+            $this->assertEquals("33", $result);
+        }
+        function test_findScore_invalidCharacters()
+        {
+            //Arrange
+            $test_Scrabble = new Scrabble;
+            $user_word = "zjkypgt!!";
+
+            //Act
+            $result = $test_Scrabble->findScore($user_word);
+
+            //Assert
+            $this->assertEquals('This is not a word', $result);
+        }
+
+
 
 
 
