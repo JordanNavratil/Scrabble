23f7c6df2ae90844f0f71f5a684d9d284819f010
Jason Bethel & jordan navratil <student@epicodus.com>
2015-08-13 10:07:43 -0700

    Added to specs

---
+++ src/Scrabble.php
@@ -0,0 +1,10 @@
+<?php
+    class Scrabble
+    {
+        fucntion findScore($word)
+        {
+
+        }
+    }
+
+ ?>
---
+++ tests/ScrabbleTest.php
@@ -0,0 +1,12 @@
+<?php
+    require_once "src/Scrabble.php"
+
+    class ScrabbleTest extends PHPUnit_Framework_TestCase
+    {
+        function test_findScore_oneLetter()
+        {
+            
+        }
+
+    }
+ ?>
