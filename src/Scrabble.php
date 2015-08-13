4e0c148d9996fb16d30eee5d286b6cd8c6a303d2
Jason Bethel & jordan navratil <student@epicodus.com>
2015-08-13 11:24:17 -0700

    modify method to meet 5th test

--- src/Scrabble.php
+++ src/Scrabble.php
@@ -7,6 +7,7 @@
             $group2 = ['d', 'g'];
             $group3 = ['b', 'c', 'm', 'p'];
             $group4 = ['f', 'h', 'v', 'w', 'y'];
+            $group5 = ['k'];
             $result = 0;
             $user_letters = str_split($user_word);
 
@@ -19,6 +20,9 @@
                     $result = $result +3;
                 } elseif (in_array($letter, $group4)) {
                     $result = $result +4;
+                } elseif (in_array($letter, $group)) {
+                    $result = $result +5;
+
                 }
 
 
